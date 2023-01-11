65e0d4574944822a1c194107ad4aadab87ca2fdf
Katballe <mathias8484@live.dk>
2023-01-11 15:40:06 +0100

    Added comments to patient class

--- BurnREE/src/Patient.java
+++ BurnREE/src/Patient.java
@@ -1,8 +1,21 @@
+/**
+ * Represents a Patient with TBSA, PBD, and BSA values
+ */
 public class Patient {
-    double TBSA;
-    int PBD;
-    double BSA;
+    // Total Body Surface Area
+    public double TBSA;
+    // Percentage of Body Degree
+    public int PBD;
+    // Body Surface Area
+    public double BSA;
 
+    /**
+     * Constructor to initialize a Patient object
+     * 
+     * @param TBSA Total Body Surface Area
+     * @param PBD  Percentage of Body Degree
+     * @param BSA  Body Surface Area
+     */
     Patient(double TBSA, int PBD, double BSA) {
         this.TBSA = TBSA;
         this.PBD = PBD;
